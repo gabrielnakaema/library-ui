70eb44eb13f405223b1e5d8a96305cbbf5c983f1
Gabriel Nakaema <61115384+gabrielnakaema@users.noreply.github.com>
2021-05-21 10:31:36 -0300

    Model view tables (#1)
    * change tables to Qt's model-view approach

--- database.py
+++ database.py
@@ -1,33 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime, Boolean
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
+from PyQt5.QtSql import QSqlDatabase
 
-
-engine = create_engine('sqlite:///library.db', echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
-
-
-class Withdrawal(Base):
-    __tablename__ = 'withdrawals'
-    id = Column(Integer, primary_key=True)
-    book_barcode = Column(String, ForeignKey('books.barcode'))
-    book = relationship('Book')
-    student_name = Column(String)
-    student_grade = Column(String)
-    withdrawal_date = Column(DateTime)
-    is_returned = Column(Boolean)
-    return_date = Column(DateTime)
-
-
-class Book(Base):
-    __tablename__ = 'books'
-    barcode = Column(String, primary_key=True, unique=True)
-    title = Column(String)
-    author = Column(String)
-    amount_available = Column(Integer)
-    amount_borrowed = Column(Integer)
-
-
-Base.metadata.create_all(engine)
+connection = QSqlDatabase.addDatabase("QSQLITE")
+connection.setDatabaseName("library.sqlite")
